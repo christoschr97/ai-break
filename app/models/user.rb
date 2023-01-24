d54761520129fd571ca51594fdce6f377b2738e7
christos christodoulou <christoschristodoulou3@gmail.com>
2023-01-24 12:00:36 +0200

    adding navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :articles
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  before_save { self.username = username.downcase }
+
 end
